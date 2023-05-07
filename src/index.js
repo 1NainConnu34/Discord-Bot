9381c9d3ce09467e91ef3037e406af7658395b04
Alexandre Bret <alexandre.bret@epitech.eu>
2023-05-07 23:37:28 +0200

    added slash command when the bot join a new server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const { Client, IntentsBitField } = require('discord.js');
 const eventHandler = require('./handlers/eventHandler');
+const areCommandsDifferent = require('./utils/areCommandsDifferent');
+const getApplicationCommands = require('./utils/getApplicationCommands');
+const getLocalCommands = require('./utils/getLocalCommands');
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildVoiceStates,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
@@ -16,3 +20,53 @@ eventHandler(client);
 client.login(process.env.TOKEN);
 
 module.exports = client;
+
+client.on("guildCreate", async (newGuild) => {
+  try {
+      const localCommands = getLocalCommands();
+      const applicationCommands = await getApplicationCommands(
+          client,
+          newGuild.id
+      );
+
+      for (const localCommand of localCommands) {
+          const { name, description, options} = localCommand;
+
+          const existingCommand = await applicationCommands.cache.find(
+              (cmd) => cmd.name === name
+          );
+
+          if (existingCommand) {
+              if (localCommand.deleted) {
+                  await applicationCommands.delete(existingCommand.id);
+                  console.log(`Deleted command "${name}".`);
+                  continue;
+              }
+
+              if (areCommandsDifferent(existingCommand, localCommand)) {
+                  await applicationCommands.edit(existingCommand.id, {
+                      description,
+                      options,
+                  });
+
+                  console.log(`Edited command "${name}".`);
+              }
+          } else {
+              if (localCommand.deleted) {
+                  console.log(`Skipping registering command "${name}" as it's set to delete.`);
+                  continue;
+              }
+
+              await applicationCommands.create({
+                  name,
+                  description,
+                  options,
+              })
+
+              console.log(`Registered command "${name}".`);
+          }
+      }
+  } catch (error) {
+      console.log(`There was an error: ${error}`);
+  }
+})
